7d28a6dc59e8f4e1ba0397c188276a0a12d714e9
Vova <didjen.game@gmail.com>
2019-06-08 12:47:49 +0300

    code style: to 2 whitespaces

--- C#/C#/SignalRChat/MyHub.cs
+++ C#/C#/SignalRChat/MyHub.cs
@@ -4,24 +4,29 @@ using SignalRChat.Model.Dto;
 using System.Collections.Generic;
 
 namespace SignalRChat
-{ 
-    public class MyHub : Hub
+{
+  public class MyHub : Hub
+  {
+    private Dictionary<int, Position> _startPositions =
+        new Dictionary<int, Position> { { 1, new Position(1, 1, 1) }, { 2, new Position(2, 2, 2) } };
+
+    public void Create(UserDto user)
     {
-        private Dictionary<int, Position> _startPositions =
-            new Dictionary<int, Position> { { 1, new Position(1, 1, 1) }, { 2, new Position(2, 2, 2) } };
-
-        public void Create(UserDto user)
-        {
-            GameContext.Instance.Users.Add(user);
-            // Call the broadcastMessage method to update clients.
-            GameBroadcast.Instance.Update();
-        }
-
-        public void Update(UserDto user)
-        {
-            var curUser = GameContext.Instance.Users.Find(item => item.Name == user.Name);
-            curUser.Clone(user);
-            GameBroadcast.Instance.Update();
-        }
+      GameContext.Instance.Users.Add(user);
+      // Call the broadcastMessage method to update clients.
+      GameBroadcast.Instance.Update();
     }
+
+    public void Update(UserDto user)
+    {
+      var curUser = GameContext.Instance.Users.Find(item => item.Name == user.Name);
+      curUser.Clone(user);
+      GameBroadcast.Instance.Update();
+    }
+
+    public void Test()
+    {
+      GameBroadcast.Instance.Broadcast(null);
+    }
+  }
 }
