20db56502d3887dd1cfe7643feaf1baf0a13fd36
Vova <didjen.game@gmail.com>
2019-06-08 18:59:23 +0300

    Fix SpellDto on client

--- Client/Assets/Scripts/Model/Dto/SpellDto.cs
+++ Client/Assets/Scripts/Model/Dto/SpellDto.cs
@@ -1,8 +1,4 @@
-﻿using SignalRChat.Model.MagicFolders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Assets.Scripts.Model.MagicFolder;
 
 namespace SignalRChat.Model.Dto
 {
