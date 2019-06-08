77667ce114a47d64c6271d21c5eee9ec3a9956a7
VShev <viky2503@gmail.com>
2019-06-08 11:43:04 +0300

    roomUpdateDto

---
+++ C#/C#/SignalRChat/Model/Dto/RoomUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRChat.Model.Dto
+{
+    public class RoomUpdateDto
+    {
+        public Dictionary<int, Room> Rooms;
+    }
+}
